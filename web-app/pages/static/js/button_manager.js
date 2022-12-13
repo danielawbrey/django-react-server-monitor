e0862de61266293462e95babfe2fdeea84718342
Daniel Awbrey <danielawbrey@gmail.com>
2022-12-13 01:47:38 -0800

    parsing user inputs

--- web-app/pages/static/js/button_manager.js
+++ web-app/pages/static/js/button_manager.js
@@ -1,33 +1,59 @@
-  function alert_user() {
-    let container = document.getElementById('website_form');
+let input_counter = 1;
 
-    let web_address_label = document.createElement("label");
-    web_address_label.innerHTML = "Website";
-    container.append(web_address_label);
+window.onload = function() {
+  create_line_graph();
+};
 
-    let web_address_input = document.createElement("input");
-    web_address_input.setAttribute("type", "text");
-    web_address_input.setAttribute("name", "FullName");
-    container.append(web_address_input);
+function add_form_elements() {
+  input_counter++;
 
-    let web_port_label = document.createElement("label");
-    web_port_label.innerHTML = "Port";
-    container.append(web_port_label);
+  create_label("Website");
+  create_input("website_address", input_counter);
 
-    let web_port_input = document.createElement("input");
-    web_port_input.setAttribute("type", "text");
-    web_port_input.setAttribute("name", "FullName");
-    container.append(web_port_input);
+  create_label("Port");
+  create_input("website_port", input_counter);
 
-    create_line_graph()
+  create_line_graph()
 }
 
-function create_label() {}
+function create_label(label_text) {
+  let container = document.getElementById('website_form');
+  let web_address_label = document.createElement("label");
+  web_address_label.innerHTML = label_text;
+  container.append(web_address_label);
+}
 
-function create_input() {}
+function create_input(input_type, input_counter) {
+  let container = document.getElementById('website_form');
+  let web_address_input = document.createElement("input");
+  web_address_input.setAttribute("type", "text");
+  container.append(web_address_input);
+}
 
 function start_test() {
-  alert('Button click')
+  let user_input_arr = get_user_input();
+  console.log(parse_user_input(user_input_arr));
+}
+
+function get_user_input() {
+  let user_input = [];
+  let container = document.getElementById('website_form');
+  for (let i = 0; i < container.length; i++) {
+    user_input.push(container.elements[i].value)
+  }
+  return user_input;
+}
+
+function parse_user_input(user_input_arr) {
+  let parsed_input_arr = [], webpage_details_subarr = [];
+  for (let i = 0; i < user_input_arr.length; i++) {
+    webpage_details_subarr.push(user_input_arr[i]);
+    if(webpage_details_subarr.length % 2 == 0) {
+      parsed_input_arr.push(webpage_details_subarr);
+      webpage_details_subarr = [];
+    }
+  }
+  return parsed_input_arr;
 }
 
 function create_line_graph() {
