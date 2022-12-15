57bddc484f5b9135140215f275b3cc3c04c47dc9
Daniel Awbrey <danielawbrey@gmail.com>
2022-12-14 23:13:24 -0800

    Time series streaming

--- web-app/pages/static/js/button_manager.js
+++ web-app/pages/static/js/button_manager.js
@@ -3,22 +3,43 @@ function addFormElements() {
   let childDiv = document.createElement("div");
   container.append(childDiv);
 
-  createLabel("Website", childDiv);
-  createLabel("Uptime", childDiv);
-  createLabel("Metering", childDiv);
+  let userInput = getUserInput();
+
+  createLabel("Server Address", userInput[2] + ":" + userInput[3], childDiv);
+  createLabel("Uptime", userInput[1], childDiv);
+  createLabel("Metering", userInput[1], childDiv);
+  // createTable("Metering", userInput[0], childDiv);
   createLineGraph(childDiv);
   addGraphDeleteButton(childDiv);
 }
 
-function createLabel(labelText, childDiv) {
+function createTable(labelText, labelData, childDiv) {
+  let webAddressLabel = document.createElement("table");
+
+  let header = webAddressLabel.createTHead();
+  let row = header.insertRow(0);
+
+  let cell = NaN;
+
+  let headerArr = ["Server Address", "Uptime", "Metering"];
+  for(let i = 0; i < 3; i++) {
+    cell = row.insertCell();
+    cell.innerHTML = headerArr[i];
+  }
+
+  childDiv.append(webAddressLabel);
+}
+
+function createLabel(labelText, labelData, childDiv) {
   let webAddressLabel = document.createElement("label");
-  webAddressLabel.innerHTML = labelText;
+  webAddressLabel.innerHTML = labelText + ":" + labelData;
   childDiv.append(webAddressLabel);
 }
 
 function startTest() {
   let userInputArr = getUserInput();
-  console.log(parseUserInput(userInputArr));
+  console.log(userInputArr);
+  // console.log(parseUserInput(userInputArr));
   httpGetAsync();
 }
 
@@ -31,17 +52,17 @@ function getUserInput() {
   return userInput;
 }
 
-function parseUserInput(userInputArr) {
-  let parsedInputArr = [], webpageDetailsSubarr = [];
-  for (let i = 0; i < userInputArr.length; i++) {
-    webpageDetailsSubarr.push(userInputArr[i]);
-    if(webpageDetailsSubarr.length % 2 == 0) {
-      parsedInputArr.push(webpageDetailsSubarr);
-      webpageDetailsSubarr = [];
-    }
-  }
-  return parsedInputArr;
-}
+// function parseUserInput(userInputArr) {
+//   let parsedInputArr = [], webpageDetailsSubarr = [];
+//   for (let i = 0; i < userInputArr.length; i++) {
+//     webpageDetailsSubarr.push(userInputArr[i]);
+//     if(webpageDetailsSubarr.length % 4 == 0) {
+//       parsedInputArr.push(webpageDetailsSubarr);
+//       webpageDetailsSubarr = [];
+//     }
+//   }
+//   return parsedInputArr;
+// }
 
 function httpGetAsync() {
   let xmlHttp = new XMLHttpRequest();
@@ -57,35 +78,64 @@ function httpGetAsync() {
 
 function drawChart(container) {
   container.height = 50;
-  new Chart(container, {
+  const chart = new Chart(container, {
     type: 'line',
-    data: {
-      labels: [100, 200, 300, 400, 500],
-      datasets: [{ 
-          data: [86,114,106,106,107,111,133,221,483,278],
-          label: "Africa",
-          borderColor: "#3e95cd",
-          fill: true
-        }
-      ]
-    },
     options: {
+      plugins: {
+        // Change options for ALL axes of THIS CHART
+        streaming: {
+          duration: 20000
+        }
+      },
       scales: {
-        yAxes: [{
+        xAxes: {
+          type: 'realtime',
+          realtime: {
+            duration: 20000
+          },
           scaleLabel: {
-            display: true,
-            labelString: 'HTTP Status'
-          }
-        }],
-        xAxes: [{
-          scaleLabel: {
+              display: true,
+              labelString: 'Time (ms)'
+          },
+        }
+      },
+      yAxes: {
+        scaleLabel: {
             display: true,
             labelString: 'Time (ms)'
-          }
-        }]
-      }
+        },
+      },
     }
-    });
+  });
+  // new Chart(container, {
+  //   type: 'line',
+  //   data: {
+  //     labels: [100, 200, 300, 400, 500],
+  //     datasets: [{ 
+  //         data: [86,114,106,106,107,111,133,221,483,278],
+  //         label: "Africa",
+  //         borderColor: "#3e95cd",
+  //         fill: true
+  //       }
+  //     ]
+  //   },
+  //   options: {
+  //     scales: {
+  //       yAxes: [{
+  //         scaleLabel: {
+  //           display: true,
+  //           labelString: 'HTTP Status'
+  //         }
+  //       }],
+  //       xAxes: [{
+  //         scaleLabel: {
+  //           display: true,
+  //           labelString: 'Time (ms)'
+  //         }
+  //       }]
+  //     }
+  //   }
+  //   });
 }
 
 function addGraphDeleteButton(childDiv) {
